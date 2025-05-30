3ad615f3a14c40971c7b5be860193a37cb6971a6
JonathanChacon <tyflosaccessiblesoftware@gmail.com>
2025-05-30 14:30:27 +0200

    Initial Commit

---
+++ Code File Catcher/Code_File_CatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_File_CatcherApp.swift
+//  Code File Catcher
+//
+//  Created by Jonathan Chacón Barbero on 30/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Code_File_CatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code File Catcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Code File Catcher
+//
+//  Created by Jonathan Chacón Barbero on 30/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
